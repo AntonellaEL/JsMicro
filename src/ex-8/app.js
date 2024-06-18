48c444d14917f1d552b713a13c253560106c3dc3
Antonella Enriquez Ledesma <antonellaenriquezledesma@gmail.com>
2024-06-18 19:51:18 +0200

    ej 8 fixed

--- src/ex-8/app.js
+++ src/ex-8/app.js
@@ -1,14 +1,15 @@
-document.getElementById('cont').addEventListener('click', function() {
-    let palabra = document.getElementById('frase').value; 
-    let total = 0;
+document.getElementById('mostrarVocales').addEventListener('click', function() {
+    let frase = document.getElementById('frase').value; 
+    let vocalesEncontradas = [];
     const vocales = new Set(['a', 'e', 'i', 'o', 'u']);
 
-    for (let i = 0; i < palabra.length; i++) {
-        if (vocales.has(palabra[i].toLowerCase())) { 
-            total++;
+    for (let i = 0; i < frase.length; i++) {
+        let letra = frase[i].toLowerCase();
+        if (vocales.has(letra) && !vocalesEncontradas.includes(letra)) { 
+            vocalesEncontradas.push(letra);
         }
     }
 
     let parrafo = document.getElementById('resultado');
-    parrafo.innerHTML = `Hay ${total} vocales en la frase.`;
+    parrafo.innerHTML = `Las vocales en la frase son: ${vocalesEncontradas.join(', ')}`;
 });
